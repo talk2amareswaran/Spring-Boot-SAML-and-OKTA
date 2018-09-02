34bd272158df08437835d9e55f1bf6f17ce658c9
Amareswaran <32594660+talk2amareswaran@users.noreply.github.com>
2018-09-02 23:19:47 +0530

    Spring Boot, SAML and OKTA

---
+++ src/main/java/com/talk2amareswaran/projects/samlapp/SamlappApplication.java
@@ -0,0 +1,20 @@
+package com.talk2amareswaran.projects.samlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class SamlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlappApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
